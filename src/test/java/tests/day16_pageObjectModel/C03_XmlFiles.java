44c1a092958a965fc1825b8397b54848840186fc
BICER <3719.bicer@gmail.com>
2023-01-25 12:06:46 +0300

    C03_XmlFiles

---
+++ src/test/java/tests/day16_pageObjectModel/C03_XmlFiles.java
@@ -0,0 +1,31 @@
+package tests.day16_pageObjectModel;
+
+import org.testng.annotations.Test;
+
+public class C03_XmlFiles {
+
+    @Test
+    public void Test06(){
+        System.out.println("test 6 calisti");
+    }
+
+    @Test
+    public void Test07(){
+        System.out.println("test 7 calisti");
+    }
+
+    @Test
+    public void Test08(){
+        System.out.println("test 8 calisti");
+    }
+
+    @Test
+    public void Test09(){
+        System.out.println("test 9 calisti");
+    }
+
+    @Test
+    public void Test10(){
+        System.out.println("test 10 calisti");
+    }
+}
