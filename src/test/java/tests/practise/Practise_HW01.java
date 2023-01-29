8cddea7c8258d62245209d0fc6b230c92f3d975c
BICER <3719.bicer@gmail.com>
2023-01-29 23:51:24 +0300

    Practise_HW01

---
+++ src/test/java/tests/practise/Practise_HW01.java
@@ -0,0 +1,13 @@
+package tests.practise;
+
+public class Practise_HW01 {
+
+    /*
+     * Navigate to https://www.saucedemo.com/
+     * Enter the username as standard_user
+     * Enter the password as secret_sauce
+     * Click on login button
+     *   `Test1 : Choose price low to high
+     *    Test2 : Verify item prices are sorted from low to high with soft Assert
+     */
+}
