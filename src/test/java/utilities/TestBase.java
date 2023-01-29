4079d972aa3766c4cc189681c2f2fc600cd6ea31
BICER <3719.bicer@gmail.com>
2023-01-29 23:53:14 +0300

    TestBase

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,48 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+public class TestBase {
+
+    protected WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void teardown(){
+        driver.close();
+    }
+    public void wait(int saniye){
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void fotografCek(String eurozoneResmi){
+        TakesScreenshot tss= (TakesScreenshot) driver;
+        File resimDosyasi= new File("target/screnShots/tumSayfaResmi.jpeg");
+        File geciciResim=tss.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(geciciResim,resimDosyasi);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
